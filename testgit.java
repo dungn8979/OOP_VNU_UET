ca42ba82af8187d799cd511e9058fd457bbd28e5
dungn8979 <42002259+dungn8979@users.noreply.github.com>
2021-09-15 22:44:31 +0700

    Update testgit.java

--- testgit.java
+++ testgit.java
@@ -1,5 +1,10 @@
 public class testgit{
     public static void main(String[] args){
         System.out.print("oop_vnu_uet");
+        PrintFor();
+    }
+    public void PrintFor(){
+        for(int i=0; i<10; i++){
+            System.out.print(i);
     }
 }
